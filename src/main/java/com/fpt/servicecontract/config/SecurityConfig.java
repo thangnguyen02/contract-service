91d59ff5ee4edb26bd1af6c470b486191935a628
nam0612 <blackswanbesidelake@gmail.com>
2024-06-09 08:31:38 +0700

    upcode

--- src/main/java/com/fpt/servicecontract/config/SecurityConfig.java
+++ src/main/java/com/fpt/servicecontract/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
             .requestMatchers("/swagger-ui/**").permitAll()
             .requestMatchers("/api-docs/**").permitAll()
             .requestMatchers("/admin/**").hasAnyRole(ADMIN.name())
-                .requestMatchers("/notification/**").permitAll()
+                .requestMatchers("/pdf/**").permitAll()
 //            .requestMatchers("/manager/**").hasAnyRole(ADMIN.name(), MANAGER.name())
             .anyRequest().authenticated()).sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
 
