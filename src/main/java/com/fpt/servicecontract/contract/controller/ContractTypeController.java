9d3a7d71ff1def50b0630e279d86fe09734cd6bb
tudda <tentufancr7@gmail.com>
2024-07-09 00:08:30 +0700

    add description contract

--- src/main/java/com/fpt/servicecontract/contract/controller/ContractTypeController.java
+++ src/main/java/com/fpt/servicecontract/contract/controller/ContractTypeController.java
@@ -1,19 +1,14 @@
 package com.fpt.servicecontract.contract.controller;
 
-import com.fpt.servicecontract.contract.dto.CreateUpdateOldContract;
 import com.fpt.servicecontract.contract.model.ContractType;
 import com.fpt.servicecontract.contract.service.ContractTypeService;
-import com.fpt.servicecontract.utils.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.Optional;
 
 @Slf4j
