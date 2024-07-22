7bf10acd9e98d08f1da1dca6562f226b5e2f8bf4
nam0612 <nguyenhainammoonligh@gmail.com>
2024-07-22 20:24:41 +0700

    upcode

--- src/main/java/com/fpt/servicecontract/contract/service/impl/PaySlipServiceImpl.java
+++ src/main/java/com/fpt/servicecontract/contract/service/impl/PaySlipServiceImpl.java
@@ -45,7 +45,7 @@ public class PaySlipServiceImpl implements PaySlipService {
         for (String email : saleEmails) {
             Double numberSale = DataUtil.isNullObject(saleAndNumberSalesAll.get(email)) ? 0 : saleAndNumberSalesAll.get(email);
             Optional<PaySlipFormula> formulaOptional = paySlipFormulas.stream().filter(e ->
-                 numberSale >= e.getFromValueContract() && numberSale <= e.getToValueContract()
+                 numberSale >= e.getFromValueContract() && numberSale < e.getToValueContract()
             ).findFirst();
             if (formulaOptional.isPresent()) {
                 PaySlipFormula formula = formulaOptional.get();
